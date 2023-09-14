7d0535e80ca6a39da533a081498c5f74c7280cc8
sakebu9 <wmora185@gmail.com>
2023-09-14 14:39:16 -0500

    Update

---
+++ src/main/java/com/proyecto/demo/dominian/camasEntity.java
@@ -0,0 +1,17 @@
+package com.proyecto.demo.dominian;
+
+import javax.persistence.Entity;
+
+@Entity
+public class camasEntity {
+    private String nombre ;
+
+    private String size;
+
+    private String tipo;
+
+    private String precio;
+
+    private String color;
+
+}
