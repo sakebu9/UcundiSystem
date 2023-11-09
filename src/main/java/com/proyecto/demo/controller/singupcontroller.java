b62239dc22738db12562178b2737e0acc57f9cf9
sakebu9 <wmora185@gmail.com>
2023-11-09 11:15:54 -0500

    update

--- src/main/java/com/proyecto/demo/controller/singupcontroller.java
+++ src/main/java/com/proyecto/demo/controller/singupcontroller.java
@@ -1,18 +1,37 @@
 package com.proyecto.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.proyecto.demo.Repositories.userRepositories;
+import com.proyecto.demo.constans.loginCodes;
+import com.proyecto.demo.constans.loginUtils;
+import com.proyecto.demo.service.UserService;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/sing")
 public class singupcontroller {
     @Autowired
-    private userRepositories UserDao;
+    private UserService userService;
 
 
-    public String singupcontroller(){
-      return("works");
+    
+      
+    @PostMapping("/signup")
+    public ResponseEntity<String> registroUsuario(@RequestBody(required = true)Map<String,String> requestMap){
+        try{
+            userService.SignUp(requestMap);
+            return loginUtils.getResponseEntity(loginCodes.SUCCESS, HttpStatus.CREATED);
+        }catch (Exception exception){
+            exception.printStackTrace();
+        }
+        return loginUtils.getResponseEntity(loginCodes.SOMETHING_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-}
+    }
+
