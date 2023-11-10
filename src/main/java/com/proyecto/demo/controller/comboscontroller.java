214b9967f6b075fbb623d8c7962484064c5403c2
sakebu9 <wmora185@gmail.com>
2023-11-10 11:23:45 -0500

    update

--- src/main/java/com/proyecto/demo/controller/comboscontroller.java
+++ src/main/java/com/proyecto/demo/controller/comboscontroller.java
@@ -14,7 +14,7 @@ import com.proyecto.demo.Repositories.combosRepositories;
 @RestController
 @RequestMapping("/api")
 public class comboscontroller {
-      private final combosRepositories comboRepo;
+    private final combosRepositories comboRepo;
 
    @Autowired
     public comboscontroller(combosRepositories comboRepo) {
